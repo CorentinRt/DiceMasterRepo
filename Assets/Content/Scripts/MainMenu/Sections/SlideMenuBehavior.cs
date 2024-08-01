41fe312dd1fbb830c175fccf8cd6281c721effb9
CorentinRmt <corentin.remot@gmail.com>
2024-08-01 18:07:33 +0200

    [UPDATE] create all sections + start play section

--- Assets/Content/Scripts/MainMenu/Sections/SlideMenuBehavior.cs
+++ Assets/Content/Scripts/MainMenu/Sections/SlideMenuBehavior.cs
@@ -1,22 +1,53 @@
+using DG.Tweening;
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SlideMenuBehavior : MonoBehaviour
 {
     #region Fields
-    [SerializeField] private List<Vector3> _positions;
+    [SerializeField] private RectTransform _slideAnchor;
 
+    [SerializeField] private List<RectTransform> _positions;
 
+    [SerializeField] private float _transitionTime;
 
     #endregion
 
+    #region Check PlayMode
+    private bool IsPlayMode()
+    {
+#if UNITY_EDITOR
+        return Application.isPlaying;
+#else
+            return false;
+#endif
+    }
+    #endregion
 
-    private void SlideTo(int indexPos)
+    #region Test Slide
+    [Button, ShowIf("IsPlayMode")]
+    private void SlideToShop() => SlideTo(0);
+    [Button, ShowIf("IsPlayMode")]
+    private void SlideToCollection() => SlideTo(1);
+    [Button, ShowIf("IsPlayMode")]
+    private void SlideToPlay() => SlideTo(2);
+    [Button, ShowIf("IsPlayMode")]
+    private void SlideToEvents() => SlideTo(3);
+    [Button, ShowIf("IsPlayMode")]
+    private void SlideToNews() => SlideTo(4);
+    #endregion
+
+    #region Slide
+    public void SlideTo(int indexPos)
     {
         if (indexPos >= _positions.Count)
             return;
 
-
+        Debug.Log("Slide");
+        _slideAnchor.DOLocalMoveX(-_positions[indexPos].localPosition.x, _transitionTime);
     }
+    #endregion
 }
